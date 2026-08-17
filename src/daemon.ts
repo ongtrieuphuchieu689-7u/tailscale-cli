acfce64a659eb4f7b22be53c40be4b0d1e6c4c81
ongtrieuhau <o.ngtrieuhau861@gmail.com>
2026-08-17 07:37:07 +0700

    fix(daemon): configure state directory for HTTPS certificates

--- src/daemon.ts
+++ src/daemon.ts
@@ -108,6 +108,7 @@ async function startUserspaceDaemon(): Promise<{
   const args = [
     "--tun=userspace-networking",
     `--state=${state}`,
+    `--statedir=${dirname(state)}`,
     `--socket=${socket}`,
   ];
   for (const path_ of [dirname(state), dirname(socket)]) {
